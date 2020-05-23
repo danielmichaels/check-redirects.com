5a639a1a9ee36b38495d67d8b985debb10f4f204
danielmichaels <dans.address@outlook.com>
2020-05-23 20:47:15 +1000

    wtform URL validation added

--- app/blueprints/page/forms.py
+++ app/blueprints/page/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField
-from wtforms.validators import DataRequired, Length
+from wtforms.validators import DataRequired, Length, URL
 
 
 class SearchForm(FlaskForm):
-    search = StringField("Which URL to check", [DataRequired(), Length(3, 512)])
+    search = StringField("Which URL to check", [DataRequired(), Length(3, 512), URL(require_tld=True, message="Must provide a URL")])
