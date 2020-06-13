a807b8a701fd0fa5332e8a6623006ed4fac24cac
danielmichaels <dans.address@outlook.com>
2020-06-13 14:23:04 +1000

    Docstrings test tasks

--- app/tests/contact/test_tasks.py
+++ app/tests/contact/test_tasks.py
@@ -3,9 +3,12 @@ from app.extensions import mail
 
 
 class TestTasks(object):
+    """ Test the mail suite. """
+
     def test_deliver_support_email(self):
         """ Deliver a contact email. """
-        form = {"email": "foo@bar.com", "message": "Test message from Snake Eyes."}
+        form = {"email": "foo@bar.com",
+                "message": "Test message from Snake Eyes."}
 
         with mail.record_messages() as outbox:
             deliver_contact_email(form.get("email"), form.get("message"))
