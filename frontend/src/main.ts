b5bd28408458d3934472f1b5452aac3d2345b5ac
danielmichaels <dans.address@outlook.com>
2020-07-01 20:41:18 +1000

    Create boostrap plugin file and import to main

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,13 +2,16 @@ import '@babel/polyfill';
 // Import Component hooks before component definitions
 import './component-hooks';
 import Vue from 'vue';
-import './plugins/vuetify';
-import './plugins/vee-validate';
+import './plugins/vuetify'; // remove later
+import './plugins/vee-validate'; // remove later
+import './plugins/bootstrap';
 import App from './App.vue';
 import router from './router';
 import store from '@/store';
 import './registerServiceWorker';
 import 'vuetify/dist/vuetify.min.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 
 Vue.config.productionTip = false;
 
