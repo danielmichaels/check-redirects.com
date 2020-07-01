6626fcbc90a1ee0bb9f7706db3c1965cd6f76df8
danielmichaels <dans.address@outlook.com>
2020-07-01 20:59:20 +1000

    Basic navbar included at "/"

--- frontend/src/router.ts
+++ frontend/src/router.ts
@@ -11,7 +11,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      component: () => import(/* webpackChunkName: "start" */ './views/main/Start.vue'),
+      component: () => import(/* webpackChunkName: "start" */ './views/Index.vue'),
+      // component: () => import(/* webpackChunkName: "start" */ './views/main/Start.vue'),
       children: [
         {
           path: 'login',
