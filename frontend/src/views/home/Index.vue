ace6cb53466c3e84d40bc941efeff109c91d5c03
danielmichaels <dans.address@outlook.com>
2020-07-04 21:18:45 +1000

    Add props to results to allow child inheritance of response data

--- frontend/src/views/home/Index.vue
+++ frontend/src/views/home/Index.vue
@@ -7,7 +7,6 @@
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import { appName } from '@/env';
 import Navbar from '@/views/Navbar.vue';
 import Search from '@/views/home/Search.vue';
 @Component({
---
+++ frontend/src/views/home/Results.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="results" class="container-fluid">
+    <b-card class="mt-3" header="responseData from Results.vue">
+      <pre class="mt-0">
+      {{ propData }}
+          </pre
+      >
+    </b-card>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Index extends Vue {
+  @Prop({
+    type: Array,
+  })
+  propData!: string[];
+
+  constructor() {
+    super();
+    console.log(this.propData);
+  }
+}
+</script>
+
+<style scoped></style>
