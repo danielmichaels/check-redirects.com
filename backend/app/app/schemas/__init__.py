6e6db5b98c0a4ec7b6c4f677de113b165797c0df
danielmichaels <dans.address@outlook.com>
2020-07-15 20:34:23 +1000

    Revert portions of last merge.
    notes:
    - the response/error model system needs to be refactored. It currently does not
    suit the FastAPI methodology (the current system is probably wrong). In future,
    the response model should have a way to return an error in its fields and from that
    we present to user.

--- backend/app/app/schemas/__init__.py
+++ backend/app/app/schemas/__init__.py
@@ -1,5 +1,5 @@
 from .item import Item, ItemCreate, ItemInDB, ItemUpdate
 from .msg import Msg
-from .redirect import Response
+from .redirect import Response, ResponseError, StatusResponse, ErrorReasons
 from .token import Token, TokenPayload
 from .user import User, UserCreate, UserInDB, UserUpdate
